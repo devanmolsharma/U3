b1e747dc8c898a6ad651e550f45aa35447a1b918
Anmol Sharma <keepupsharma@gmail.com>
2024-01-31 14:57:02 -0600

    first commit

--- challenge_read.rb
+++ challenge_read.rb
@@ -3,3 +3,12 @@ require_relative 'ar.rb'
 number_of_products = Product.count
 
 puts "There are #{number_of_products} in the products table."
+
+filtered_count = Product.where('name LIKE "C%" and price > 10').count;
+
+puts "There are #{filtered_count} products above $10 with names that begin with the letter C in the table."
+
+low_stock_count = Product.where('stock_quantity < 5').count;
+
+puts "There are #{low_stock_count} products low in stock."
+
--- models/product.rb
+++ models/product.rb
@@ -1,6 +1,10 @@
 class Product < ActiveRecord::Base
   # This AR object is linked with the products table.
-  
+  #<Product id: 155, name: "Chai", description: "10 boxes x 30 bags", price: 0.18e2, stock_quantity: 39, category_id: 1, created_at: "2011-11-18 04:48:44.959733000 +0000", updated_at: "2011-11-18 04:48:44.959733000 +0000">
+
+  validates :name,:description,:price,:stock_quantity,:category, presence: true
+  validates :name,uniqueness: true , length: { minimum: 5 }
+
   # A product has a many to one relationship with a category.
   # The products table has a category_id foreign key.
   # In other words, a product belongs to a category.
